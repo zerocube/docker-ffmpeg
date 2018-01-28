0e58fe6270f35ea04fc6f1e5ea9846fcf13ec4f1
Jordan Kueh <jordan@zerocu.be>
2018-01-28 15:27:09 +1100

    Using the httpredir Debian mirror, and fixing the apk package reference for the Alpine image

--- docker-bootstrap.sh
+++ docker-bootstrap.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-apt update && apt install -y wget
+# Change mirror to use httpredir.debian.org
+sed -i 's/deb.debian.org/httpredir.debian.org/g' /etc/apt/sources.list
+
+apt-get update && apt-get install -y wget
 KEYRING_PKG="http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb"
 wget -O /tmp/keyring.deb "${KEYRING_PKG}"
 dpkg -i /tmp/keyring.deb && rm -v /tmp/keyring.deb
 
 echo 'deb http://www.deb-multimedia.org stable main non-free' > /etc/apt/sources.list.d/deb-multimedia.list
-apt update && apt install -y ffmpeg
+apt-get update && apt-get install -y ffmpeg
