99d3e126f440938da16963ce3d6802496b63ae50
Jordan Kueh <jordan@zerocu.be>
2018-03-18 12:33:39 +1100

    Making the docker-build script sh friendly

--- docker-build.sh
+++ docker-build.sh
@@ -1,19 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # Download image to cache from, up to three refs behind
-PREV_REFS=(
-  "$(git rev-parse --short HEAD~)"
-  "$(git rev-parse --short HEAD~~)"
-  "$(git rev-parse --short HEAD~~~)"
-)
-for REF in "${PREV_REFS[@]}"; do
+for REF in \
+  "$(git rev-parse --short HEAD~)" \
+  "$(git rev-parse --short HEAD~~)" \
+  "$(git rev-parse --short HEAD~~~)"; do
+
   PREV_IMAGE="${CI_REGISTRY_IMAGE}:${REF}"
   docker pull "${PREV_IMAGE}" && break
 done
 
-function get_git_root() {
-  git rev-parse --show-toplevel \
-  || "$(dirname "${0}")"
+get_git_root() {
+  git rev-parse --show-toplevel || dirname "${0}"
 }
 
 docker build \
